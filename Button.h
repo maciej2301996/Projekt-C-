04dbdabf7cb8b8ad0a9df054167e27c6c185f137
maciej2301996 <286032@uwr.edu.pl>
2016-12-06 00:46:00 +0100

    Add files via upload

---
+++ Button.h
@@ -0,0 +1,40 @@
+#ifndef BUTTON_H
+#define BUTTON_H
+
+#include <QGraphicsRectItem>
+#include <QGraphicsSceneMouseEvent>
+#include <QGraphicsTextItem>
+#include <QBrush>
+
+
+class Button : public QObject,public QGraphicsRectItem{
+    Q_OBJECT
+public :
+    //constructors
+    Button(QString name,QGraphicsItem* parent = 0);
+
+    //
+    ~Button();
+
+    // methods
+   void mousePressEvent(QGraphicsSceneMouseEvent *event);
+   void hoverEnterEvent(QGraphicsSceneHoverEvent *event);
+   void hoverLeaveEvent(QGraphicsSceneHoverEvent *event);
+signals:
+   void clicked();
+
+private:
+    QGraphicsTextItem* text;
+
+};
+
+
+
+
+
+
+
+
+
+
+#endif // BUTTON_H
---
+++ ProjektIPP.pro
@@ -0,0 +1,23 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2016-11-28T21:02:18
+#
+#-------------------------------------------------
+
+QT       += core gui
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = ProjektIPP
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+    Game.cpp \
+    Button.cpp
+
+HEADERS  += \
+    Button.h \
+    Game.h
+
+FORMS    += game.ui
